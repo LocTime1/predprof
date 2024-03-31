a1ae262b6e519ed1812ed4a4cfd7f4935afa3f06
LocTime1 <124242261+LocTime1@users.noreply.github.com>
2024-03-31 11:50:26 +0300

    Update зфкерштп.py

--- зфкерштп.py
+++ зфкерштп.py
@@ -2,5 +2,8 @@ import requests
 
 
 headers = {'X-Auth-Token': 'ppo_11_14610'}
-a = requests.get('https://olimp.miet.ru/ppo_it_final', headers)
-print(a)
+a = requests.get(f'https://olimp.miet.ru/ppo_it_final?day=30&month=10&year=23', headers=headers)
+
+b = a.json()['message']
+print(b['flats_count'])
+#b'{"message":["25-01-23","14-02-23","18-02-23","04-03-23","14-03-23","18-04-23","13-09-23","30-09-23","30-10-23"]}\n'
